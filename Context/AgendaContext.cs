9f3b770bc0a7231345e8255eb593eb02697bc7f4
AndersonC <138899243+Dev-AndersonC@users.noreply.github.com>
2023-12-28 00:43:47 -0200

    Initial commit

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,17 @@
+﻿using CrudMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVC.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Contato> Contatos
+        {
+        get; set; }
+    }
+}
